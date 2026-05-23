203496f427c03ac95217d7282f03884dcb8a0b7a
xicivibi <xicivibi@gmail.com>
2026-05-23 12:00:28 +0900

    Use Korean locale for blog

--- astro.config.ts
+++ astro.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["ko"],
+    defaultLocale: "ko",
     routing: {
       prefixDefaultLocale: false,
     },
